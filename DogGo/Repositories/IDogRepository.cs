ec5990196db2c9f2bd16ebcaa9aaacaa55c0eada
Micaela Colpi <micaelacolpi@gmail.com>
2021-06-28 00:29:32 -0500

    Add working for dog model

---
+++ DogGo/Repositories/IDogRepository.cs
@@ -0,0 +1,16 @@
+﻿using DogGo.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+    public interface IDogRepository
+    {
+        public List<Dog> GetAllDogs();
+        public Dog GetDogById(int id);
+        void AddDog(Dog dog);
+        void UpdateDog(Dog dog);
+        void DeleteDog(int dogId);
+    }
+}
